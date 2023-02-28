56b76c1e66592a1facd56e3be16468d1a2291447
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-02-28 09:57:18 +0530

    commit

--- src/main/java/org/example/Contact.java
+++ src/main/java/org/example/Contact.java
@@ -1,109 +1,6 @@
 package org.example;
 import java.util.Scanner;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-class Node{
-    String name;
-    String email;
-    String number;
-    Node next;
-    Node(String name,String email,String number){
-        this.name=name;
-        this.email=email;
-        this.number=number;
-        this.next=null;
-    }
-}
-class LinkedList {
-    Logger log= Logger.getLogger("Contact");
-
-    Node head;
-    int size;
-
-    LinkedList() {
-        this.head = null;
-        this.size = 0;
-    }
-
-    void insertFirst(String name, String email, String number) {
-        Node n = new Node(name, email, number);
-        if(this.head == null)
-            this.head = n;
-        else {
-            n.next = this.head;
-            this.head = n;
-        }
-    }
-    void insertLast(String name,String email,String number){
-        Node n=new Node(name,email,number);
-        Node temp;
-        if (this.head == null) {
-            this.head = n;
-        }
-        else
-        {
-            temp=this.head;
-            while(temp.next!=null){
-                temp=temp.next;
-            }
-            temp.next=n;
-            this.size++;
-        }
-    }
-    void insertAt(String name,String email,String number,int index){
-        Node n=new Node(name,email,number);
-        Node temp=this.head;
-        for (int i=1;i<index;i++){
-            temp=temp.next;
-        }
-        n.next=temp.next;
-        temp.next=n;
-    }
-    void removeFirst(){
-        Node temp=this.head;
-        this.head=temp.next;
-    }
-    void removeLast(){
-        Node temp=this.head;
-        Node prev=temp;
-        while (temp.next!=null){
-            prev=temp;
-            temp=temp.next;
-        }
-        prev.next=null;
-    }
-    void removeAt(int index){
-        Node temp=this.head;
-        Node prev=temp;
-        for (int i=0;i<index;i++){
-            prev=temp;
-            temp=temp.next;
-        }
-        prev.next=temp.next;
-    }
-    int search(String name)
-    {
-        int cnt=1;
-        Node temp =this.head;
-        while(temp!=null){
-            if(temp.name.equals(name))
-            {
-                return cnt;
-            }
-            temp=temp.next;
-            cnt++;
-        }
-        return -1;
-    }
-    void display(){
-        Node temp =this.head;
-        while(temp!=null){
-            String s="Name--"+temp.name+"\nEmail-- "+temp.email+"\nNumber--"+temp.number;
-            log.info(s);
-            temp=temp.next;
-        }
-    }
-}
 public class Contact {
     public static void main(String[] args) {
         LinkedList l=new LinkedList();
@@ -163,7 +60,8 @@ public class Contact {
                     int res=l.search(name);
                     if(res>0)
                     {
-                        log.log(Level.INFO,()->"Element is At Position :"+res);
+                        String s="Element is At Position :"+res;
+                      log.info(s);
 
                     }
                     else
@@ -177,7 +75,7 @@ public class Contact {
                         log.info("Wrong Choice !");
 
             }
-        }while(ch!=9);
+        }while(ch>0 && ch<=8);
     }
 
 }
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Node {
+    String name;
+    String email;
+    String number;
+    Node next;
+
+    Node(String name, String email, String number) {
+        this.name = name;
+        this.email = email;
+        this.number = number;
+        this.next = null;
+    }
+}
